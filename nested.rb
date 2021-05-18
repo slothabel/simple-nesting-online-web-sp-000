9cc5e8bd9e7cf2fc9eeabe0e468b84498ed475dc
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-05-17 21:10:55 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,12 +16,13 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
