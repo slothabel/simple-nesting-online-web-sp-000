9d30b0d215630b18ed3f2d3f2b87670334fac319
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-05-18 17:12:06 -0400

    commit tree cc8cd1c835b475d5e8035ca8a92316b1bf37f8a8

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -58,14 +59,12 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -82,8 +81,16 @@ def adding_matz
           :languages => ["C"]
         }
      }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash
 
-    
 end
 
 def changing_alan
@@ -104,6 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for]= alans_new_info
+     programmer_hash
      
      
 end
@@ -124,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+programmer_hash
 end
